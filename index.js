c0899bc7e48a425e96cafadeef1083701ad015b3
KellyMark <kmark602@gmail.com>
2022-12-20 14:41:45 -0500

    commit tree 41e6fe40ea9fb677990feea6f927cd9b5456557f

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+function findMatching(driversArray, stringToMatch) {
+    const matchingDrivers = [];
+     driversArray.filter(function (driver) {
+        if (driver === stringToMatch || driver === stringToMatch.toLowerCase()) {
+            matchingDrivers.push(driver);
+        }
+    })
+    return matchingDrivers;
+}
+
+function fuzzyMatch(driversArray, stringOfLetters) {
+    const fuzzyMatchingDrivers = [];
+    driversArray.filter(function (driver) {
+        if (driver[0] === stringOfLetters[0] && driver[1] === stringOfLetters[1]) {
+            fuzzyMatchingDrivers.push(driver);
+        }
+    })
+    return fuzzyMatchingDrivers;
+}
+
+function matchName(driversObjects, nameStringToMatch) {
+    const matchingDrivers = [];
+    driversObjects.filter(function(driver) {
+        if (driver.name === nameStringToMatch) {
+            matchingDrivers.push(driver);
+        }
+    })
+    return matchingDrivers;
+}   
